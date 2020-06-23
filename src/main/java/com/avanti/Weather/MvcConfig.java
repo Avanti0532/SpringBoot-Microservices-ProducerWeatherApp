536ca311bdfdbb133f0e410e95bfa4bee86df73e
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-06-22 23:35:52 -0500

    add aws sns logic to push weather data onto aws sns

--- src/main/java/com/avanti/Weather/MvcConfig.java
+++ src/main/java/com/avanti/Weather/MvcConfig.java
@@ -1,5 +1,7 @@
 package com.avanti.Weather;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,5 +11,10 @@ public class MvcConfig {
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public NotificationMessagingTemplate notificationMessagingTemplate(AmazonSNS amazonSNS){
+        return new NotificationMessagingTemplate(amazonSNS);
     }
 }
--- src/main/java/com/avanti/Weather/service/WeatherService.java
+++ src/main/java/com/avanti/Weather/service/WeatherService.java
@@ -2,11 +2,12 @@ package com.avanti.Weather.service;
 
 import com.avanti.Weather.model.Weather;
 import com.avanti.Weather.model.WeatherAlert;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.util.List;
 
 public interface WeatherService {
 
     List<WeatherAlert> getWeatherSorted();
-    Boolean postWeather(Weather weather);
+    Boolean postWeather(Weather weather) throws JsonProcessingException;
 }
